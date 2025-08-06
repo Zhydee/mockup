9c7a55055fc68c1f8ab5012de82dfd765df3f67b
zhydee <zaidifahmi91@gmail.com>
2025-08-06 22:19:20 +0800

    Initial commit with mock secret

---
+++ main.py
@@ -0,0 +1,11 @@
+# Hardcoded API key (fake)
+api_key = "AKIAFAKEKEY1234567890"
+
+# Hardcoded password
+db_password = "SuperSecret123!"
+
+# AWS Secret Access Key pattern
+aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# Generic token
+auth_token = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
