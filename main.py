1357e02f2c00d1f51c28c8bf095078a072800154
Zaidi Fahmi <67587985+Zhydee@users.noreply.github.com>
2025-08-09 09:19:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 # Generic token
 auth_token = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+# High-confidence secret for testing
+rsa_private_key = """
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEA0R+I/2/A8s/i4QeA/iESa4vE9tS2/s/M4a4a8f8f8v8s/s4a4a
+... (mock data) ...
+-----END RSA PRIVATE KEY-----
+"""
